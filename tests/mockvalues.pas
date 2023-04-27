4a20fce4da1683d28c975fc2cc1a57523b0a1501
Andrei Tudorache <andreit@stonesouptech.com>
2023-04-27 15:38:31 +0300

    indent changes

--- tests/mockvalues.pas
+++ tests/mockvalues.pas
@@ -5,46 +5,46 @@ unit MockValues;
 interface
 
 const
-   API_USAGE_RESPONSE: string = '{' +
-      '"total": 7,' +
-      '"status_valid": 5,' +
-      '"status_invalid": 0,' +
-      '"status_catch_all": 0,' +
-      '"status_do_not_mail": 3,' +
-      '"status_spamtrap": 0,' +
-      '"status_unknown": 0,' +
-      '"sub_status_toxic": 0,' +
-      '"sub_status_disposable": 0,' +
-      '"sub_status_role_based": 0,' +
-      '"sub_status_possible_trap": 0,' +
-      '"sub_status_global_suppression": 3,' +
-      '"sub_status_timeout_exceeded": 0,' +
-      '"sub_status_mail_server_temporary_error": 0,' +
-      '"sub_status_mail_server_did_not_respond": 0,' +
-      '"sub_status_greylisted": 0,' +
-      '"sub_status_antispam_system": 0,' +
-      '"sub_status_does_not_accept_mail": 0,' +
-      '"sub_status_exception_occurred": 0,' +
-      '"sub_status_failed_syntax_check": 0,' +
-      '"sub_status_mailbox_not_found": 0,' +
-      '"sub_status_unroutable_ip_address": 0,' +
-      '"sub_status_possible_typo": 0,' +
-      '"sub_status_no_dns_entries": 0,' +
-      '"sub_status_role_based_catch_all": 0,' +
-      '"sub_status_mailbox_quota_exceeded": 0,' +
-      '"sub_status_forcible_disconnect": 0,' +
-      '"sub_status_failed_smtp_connection": 0,' +
-      '"sub_status_mx_forward": 0,' +
-      '"sub_status_alternate": 1,' +
-      '"sub_status_blocked": 0,' +
-      '"sub_status_allowed": 0,' +
-      '"start_date": "4/1/2023",' +
-      '"end_date": "4/20/2023"' +
-   '}';
-   ERROR_MESSAGE: string = 'Mock error message';
-   ERROR_PAYLOAD: string = '{"error": "Mock error message"}';
+    API_USAGE_RESPONSE: string = '{' +
+        '"total": 7,' +
+        '"status_valid": 5,' +
+        '"status_invalid": 0,' +
+        '"status_catch_all": 0,' +
+        '"status_do_not_mail": 3,' +
+        '"status_spamtrap": 0,' +
+        '"status_unknown": 0,' +
+        '"sub_status_toxic": 0,' +
+        '"sub_status_disposable": 0,' +
+        '"sub_status_role_based": 0,' +
+        '"sub_status_possible_trap": 0,' +
+        '"sub_status_global_suppression": 3,' +
+        '"sub_status_timeout_exceeded": 0,' +
+        '"sub_status_mail_server_temporary_error": 0,' +
+        '"sub_status_mail_server_did_not_respond": 0,' +
+        '"sub_status_greylisted": 0,' +
+        '"sub_status_antispam_system": 0,' +
+        '"sub_status_does_not_accept_mail": 0,' +
+        '"sub_status_exception_occurred": 0,' +
+        '"sub_status_failed_syntax_check": 0,' +
+        '"sub_status_mailbox_not_found": 0,' +
+        '"sub_status_unroutable_ip_address": 0,' +
+        '"sub_status_possible_typo": 0,' +
+        '"sub_status_no_dns_entries": 0,' +
+        '"sub_status_role_based_catch_all": 0,' +
+        '"sub_status_mailbox_quota_exceeded": 0,' +
+        '"sub_status_forcible_disconnect": 0,' +
+        '"sub_status_failed_smtp_connection": 0,' +
+        '"sub_status_mx_forward": 0,' +
+        '"sub_status_alternate": 1,' +
+        '"sub_status_blocked": 0,' +
+        '"sub_status_allowed": 0,' +
+        '"start_date": "4/1/2023",' +
+        '"end_date": "4/20/2023"' +
+    '}';
+    ERROR_MESSAGE: string = 'Mock error message';
+    ERROR_PAYLOAD: string = '{"error": "Mock error message"}';
 
-   VALDATION_RESPONSE_VALID: string = '{' +
+    VALDATION_RESPONSE_VALID: string = '{' +
         '    "address": "valid@example.com",' +
         '    "status": "valid",' +
         '    "sub_status": "",' +
@@ -66,7 +66,7 @@ const
         '    "processed_at": "2023-04-25 13:08:24.269"' +
         '}';
 
-   VALDATION_RESPONSE_INVALID: string ='{' +
+    VALDATION_RESPONSE_INVALID: string ='{' +
         '    "address": "invalid@example.com",' +
         '    "status": "invalid",' +
         '    "sub_status": "mailbox_not_found",' +
@@ -88,7 +88,7 @@ const
         '    "processed_at": "2023-12-25 13:08:24.001"' +
         '}';
 
-   BATCH_VALIDATE_OK: string = '{' +
+    BATCH_VALIDATE_OK: string = '{' +
         '    "email_batch": [' +
         '{' +
         '    "address": "valid@example.com",' +
@@ -136,12 +136,12 @@ const
         '    "errors": []' +
         '}';
 
-   BATCH_VALIDATE_ERROR_SAMPLE: string = '{' +
+    BATCH_VALIDATE_ERROR_SAMPLE: string = '{' +
         '    "email_address": "all",' +
         '    "error": "Invalid API Key or your account ran out of credits"' +
         '}';
 
-   BATCH_VALIDATE_ERROR: string = '{' +
+    BATCH_VALIDATE_ERROR: string = '{' +
         '    "email_batch": [],' +
         '    "errors": [' +
         '        {' +
@@ -152,7 +152,7 @@ const
         '}';
 
 
-        BULK_SUBMIT_OK = '{' +
+    BULK_SUBMIT_OK = '{' +
         '    "success": true,' +
         '    "message": "File Accepted",' +
         '    "file_name": "emails2.txt",' +
@@ -160,7 +160,7 @@ const
         '}';
 
 
-        BULK_STATUS_OK = '{' +
+    BULK_STATUS_OK = '{' +
         '    "success": true,' +
         '    "file_id": "e90e9b1d-8dc7-40eb-a7d9-999d52086a56",' +
         '    "file_name": "emails2.txt",' +
@@ -171,17 +171,15 @@ const
         '    "return_url": "https://mock.value.com/"' +
         '}';
 
-        BULK_RESULT_DELETED = '{ "success": false, "message": "File deleted." }';
-
-
-        BULK_DELETE_OK = '{' +
+    BULK_RESULT_DELETED = '{ "success": false, "message": "File deleted." }';
+    BULK_DELETE_OK = '{' +
         '    "success": true,' +
         '    "message": "File Deleted",' +
         '    "file_name": "emails2.txt",' +
         '    "file_id": "e90e9b1d-8dc7-40eb-a7d9-999d52086a56"' +
         '}';
 
-        BULK_DELETE_NOT_FOUND = '{ "success": false, "message": "File cannot be found."}';
+    BULK_DELETE_NOT_FOUND = '{ "success": false, "message": "File cannot be found."}';
 
 
 implementation
